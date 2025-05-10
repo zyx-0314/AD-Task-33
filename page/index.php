d2cb4c0bd053e5bd2e5d2ad8e10dadeeaf565bdf
carlmarinyo <marinojerome2@gmail.com>
2025-05-10 16:01:50 +0800

    feat (garage-page): Added PHP initializing the variables containing the car's name, brand, year, fuel type, and image

--- page/index.php
+++ page/index.php
@@ -6,7 +6,16 @@
 </head>
 <body>
     <h1>Cars in the Garage</h1>
+    <?php
+    $car_names = ["Taycan", "Mustang GT", "Civic Type R", "Cybertruck", "Landcruiser", "F250 Raptor"];
+    $car_brands = ["Porsche", "Ford", "Honda", "Tesla", "Toyota", "Ford"];
+    $car_years = [2020, 2024, 2024, 2023, 2022, 2020];
+    $car_fuel = ["Electric", "Gasoline", "Gasoline", "Electric", "Diesel", "Diesel"];
+    $car_image = ["/page/assets/img/taycan.png", "/page/assets/img/mustang.png", "/page/assets/img/civic.png", "/page/assets/img/cybertruck.png", "/page/assets/img/landcruiser.png", "/page/assets/img/f250.png"];
     
+    
+    
+    ?>
     
     <a href="/index.php" class="button">Go back to home page</a>
 </body>
