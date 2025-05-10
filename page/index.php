35cde6c83e5900e23572b5115da2007d7c9a2260
carlmarinyo <marinojerome2@gmail.com>
2025-05-10 16:08:22 +0800

    feat (garage-page): Added gasoline car section

--- page/index.php
+++ page/index.php
@@ -12,6 +12,18 @@
     $car_years = [2020, 2024, 2024, 2023, 2022, 2020];
     $car_fuel = ["Electric", "Gasoline", "Gasoline", "Electric", "Diesel", "Diesel"];
     $car_image = ["/page/assets/img/taycan.png", "/page/assets/img/mustang.png", "/page/assets/img/civic.png", "/page/assets/img/cybertruck.png", "/page/assets/img/landcruiser.png", "/page/assets/img/f250.png"];
+
+            echo "<h2>Gasoline Cars</h2>";
+        echo "<div class='container'>";    
+    for ($i = 0; $i < 6; $i++) {
+        if ($car_fuel[$i] == 'Gasoline') {
+        echo "<div class ='box'>";
+        echo "<img src = '$car_image[$i]'>";
+        echo "<p><strong>$car_brands[$i]</strong><br><br><span>$car_names[$i] ($car_years[$i])</span><br><br>$car_fuel[$i]</p>";
+        echo "</div>";
+        }
+    }
+    echo "</div>";
     
     
     
