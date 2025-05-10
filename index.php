88f20a0cee7b6dc0b541b17f69b464d560c89812
carlmarinyo <marinojerome2@gmail.com>
2025-05-10 15:00:21 +0800

    feat (landing-page): Added HTML Structure and welcome text

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Car List</title>
+    <link rel="stylesheet" href="/assets/css/example.css">
+</head>
+<body>
+<h1>Welcome to Carl's Garage!</h1>
+</body>
+</html>
