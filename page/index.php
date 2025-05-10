932572aca64f9cd3a9883cb0f417d5a244d661f6
carlmarinyo <marinojerome2@gmail.com>
2025-05-10 16:00:31 +0800

    feat (garage-page): Added HTML Structure with link back to home page

---
+++ page/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Garage</title>
+    <link rel="stylesheet" href="/page/assets/css/example.css">
+</head>
+<body>
+    <h1>Cars in the Garage</h1>
+    
+    
+    <a href="/index.php" class="button">Go back to home page</a>
+</body>
+</html>
